e73e8c7283d4b05b7d1cdf259d5f4034c68eba02
Melanie Sumner <melanie@hashicorp.com>
2023-02-21 22:26:28 -0600

    we can improve

--- js/topic-randomizer.js
+++ js/topic-randomizer.js
@@ -1,10 +1,10 @@
 function updateTopic() {
 	const topics = [
-		"apples vs oranges vs apples vs baby wolverines",
-		"oranges",
-		"frameworks",
-		"dogs",
-		"baby wolverines",
+		"'apples vs oranges' vs 'apples vs baby wolverines'",
+		"oranges used to be a prized dessert!",
+		"frameworks: you need them, get over it.",
+		"Dogs are so cute! What is the breed of the cutest dog you know IRL?",
+		"Have you watched the Great Pottery Throw Down?",
 		"numberwang!",
 		"play No More Jockeys",
 		"experienced broadcaster Peter Barakan",
@@ -16,8 +16,11 @@ function updateTopic() {
 		"When will the bird site go away? Any Bets?",
 		"latest interesting RSS feed?",
 		"why is comic sans the best font?",
-    "what is your favorite 72 hours episode on NHK World Japan?",
-    "what is the best sandwich"
+    "what is your favorite 'Document 72 hours' episode on NHK World Japan?",
+    "what is the best sandwich",
+    "what's the easiest way to put markdown into your website?",
+    "ok what podcast are you listening to and is it about murder?",
+    "what are the best headphones?"
 	];
 	let newTopicIndex = Math.floor(Math.random() * topics.length);
 	let paragraphElement = document.getElementById("topic");
